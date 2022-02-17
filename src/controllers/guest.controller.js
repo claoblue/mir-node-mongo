e915dbff79c7609a6f3ff40711d43e362b9ea437
Claudia Heredia <marcelitablue@gmail.com>
2022-02-17 18:20:58 -0500

    Obtener lista de invitados

--- src/controllers/guest.controller.js
+++ src/controllers/guest.controller.js
@@ -19,6 +19,37 @@ const createGuest = async (req, res) => {
   }
 }
 
+// GET: para obtener la lista de invitados
+const guestsList = async (req, res) => {
+  try {
+    const docs = await Guest.find()
+        .select("name type recommendation confirmed _id")
+        .exec();
+    
+    const response = {
+      count: docs.length,
+      guests: docs.map(doc => {
+        // Se hace "destructing" para traer solo los datos que necesito
+        // si hago ...doc sobre el objeto de retorno se traeran datos
+        // adicionales del documento que no son necesarios.
+        const { _id, name, foodRecommendation, confirmed } = doc;
+        return {
+          _id, name, foodRecommendation, confirmed,
+          request: {
+            type: "GET",
+            uri: `http://localhost:8081/guests/${_id}`
+          }
+        }
+      })
+    };
+    res.status(200).json(response);
+  } catch (error) {
+    console.log("err", error);
+    res.status(404).json({ message: error });
+  }
+}
+
 module.exports = {
-  createGuest
+  createGuest,
+  guestsList
 }
--- src/routes/guests.js
+++ src/routes/guests.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const guestController = require("../controllers/guest.controller");
 
 router.post("/newGuest", guestController.createGuest);
+router.get("/guestsList", guestController.guestsList);
 
 module.exports = router;
